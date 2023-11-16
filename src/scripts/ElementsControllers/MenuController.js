66c18a9c583c700c20c633b95e66115cd7b78edc
Bulat Sitdikov <bulatsitdikov@MacBook-Air.local>
2023-11-16 16:06:08 +0500

    add submenu

--- src/scripts/ElementsControllers/MenuController.js
+++ src/scripts/ElementsControllers/MenuController.js
@@ -4,6 +4,8 @@ const takeControlMenu = () => {
 	const logo = document.querySelector('#logo')
 	const bar = document.querySelector('#bar')
 	const mobileMenu = document.querySelector('#mobile-menu')
+	const subMenuButtons = document.querySelectorAll('.submenu__button')
+	const subMenu = document.querySelector('#sub-menu')
 
 	menuButton.addEventListener('click', () => {
 		menuButton.classList.toggle('close')
@@ -11,7 +13,32 @@ const takeControlMenu = () => {
 		mobileMenu.classList.toggle('active')
 		logo.classList.toggle('active')
 		bar.classList.toggle('active')
+		if (subMenu.classList.contains('active')) {
+			subMenu.classList.remove('active')
+			subMenuButtons.forEach((elem) => {
+				elem.classList.remove('active')
+			})
+		}
 		document.body.classList.toggle('lock')
+	})
+
+	subMenuButtons.forEach((elem) => {
+		elem.addEventListener('click', () => {
+			subMenuButtons.forEach((elem) => {
+				elem.classList.toggle('active')
+			})
+			subMenu.classList.toggle('active')
+			if (!document.body.classList.contains('lock')) {
+				document.body.classList.add('lock')
+			}
+
+			if (menu.classList.contains('active')) {
+				menuButton.classList.remove('close')
+				menu.classList.remove('active')
+				logo.classList.remove('active')
+				bar.classList.remove('active')
+			}
+		})
 	})
 }
 
