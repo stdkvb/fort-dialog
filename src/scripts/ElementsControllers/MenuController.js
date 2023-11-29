47a32760bf4acf5b6f76bb83231a24ed7532d5ce
Bulat Sitdikov <bulatsitdikov@MacBook-Air.local>
2023-11-29 14:58:05 +0500

    ref menu controller

--- src/scripts/ElementsControllers/MenuController.js
+++ src/scripts/ElementsControllers/MenuController.js
@@ -1,49 +1,67 @@
 const takeControlMenu = () => {
-	const menuButton = document.querySelector('#menu-button')
+	const body = document.querySelector('body')
+	const burger = document.querySelector('#burger')
+	const close = document.querySelector('#close-menu')
 	const menu = document.querySelector('#menu')
 	const logo = document.querySelector('#logo')
 	const bar = document.querySelector('#bar')
 	const mobileMenu = document.querySelector('#mobile-menu')
-	const subMenuButtons = document.querySelectorAll('.submenu__button')
+	const subMenuButton = document.querySelector('#submenu-button')
+	const closeSubMenuButton = document.querySelector('#submenu-close')
 	const subMenu = document.querySelector('#sub-menu')
 
-	menuButton.addEventListener('click', () => {
-		menuButton.classList.toggle('close')
-		menu.classList.toggle('active')
-		mobileMenu.classList.toggle('active')
-		logo.classList.toggle('active')
-		bar.classList.toggle('active')
-		if (subMenu.classList.contains('active')) {
-			subMenu.classList.remove('active')
-			subMenuButtons.forEach((elem) => {
-				elem.classList.remove('active')
-			})
+	function openMenu() {
+		burger.style.display = 'none'
+		close.style.display = 'block'
+		menu.classList.add('active')
+		mobileMenu.classList.add('active')
+		logo.classList.add('active')
+		bar.classList.add('active')
+		if (window.innerWidth <= 1800) {
+			body.classList.add('lock')
 		}
-		if (!document.body.classList.contains('lock')) {
-			document.body.classList.add('lock')
-		} else if (!subMenu.classList.contains('active')) {
-			document.body.classList.remove('lock')
+	}
+
+	function closeMenu() {
+		burger.style.display = 'block'
+		close.style.display = 'none'
+		menu.classList.remove('active')
+		mobileMenu.classList.remove('active')
+		logo.classList.remove('active')
+		bar.classList.remove('active')
+		if (window.innerWidth <= 1800) {
+			body.classList.remove('lock')
 		}
+	}
+
+	function openSubMenu() {
+		subMenu.classList.add('active')
+		subMenuButton.classList.add('active')
+	}
+
+	function closeSubMenu() {
+		subMenu.classList.remove('active')
+		subMenuButton.classList.remove('active')
+	}
+
+	burger.addEventListener('click', () => {
+		closeSubMenu()
+		openMenu()
 	})
 
-	subMenuButtons.forEach((elem) => {
-		elem.addEventListener('click', () => {
-			subMenuButtons.forEach((elem) => {
-				elem.classList.toggle('active')
-			})
-			subMenu.classList.toggle('active')
-			if (!document.body.classList.contains('lock')) {
-				document.body.classList.toggle('lock')
-			} else if (!menu.classList.contains('active')) {
-				document.body.classList.remove('lock')
-			}
-			if (menu.classList.contains('active') && window.innerWidth > 991) {
-				menuButton.classList.remove('close')
-				menu.classList.remove('active')
-				logo.classList.remove('active')
-				bar.classList.remove('active')
-			}
-		})
+	close.addEventListener('click', () => {
+		closeMenu()
+	})
+
+	subMenuButton.addEventListener('click', () => {
+		if (window.innerWidth > 991) {
+			closeMenu()
+		}
+		openSubMenu()
+	})
+
+	closeSubMenuButton.addEventListener('click', () => {
+		closeSubMenu()
 	})
 }
 
